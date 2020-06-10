03750bfb46d52e16b1f873008f61eae02619be8c
Oussama Bouguerne <oussama.bouguerne@gmail.com>
2020-06-10 11:57:38 +0100

    Integrate backend with frontend app & add cors middleware

---
+++ part3/phonebook-backend/build/precache-manifest.fc564b4f0c095066530d9b6eac110253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a33b2f4b32c7d558285f99b111867d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3c16f6b5e75aa20594b",
+    "url": "/static/css/main.2787cb23.chunk.css"
+  },
+  {
+    "revision": "6b4ed26225ace364ff75",
+    "url": "/static/js/2.b4854b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4854b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c16f6b5e75aa20594b",
+    "url": "/static/js/main.91cbd8e9.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
