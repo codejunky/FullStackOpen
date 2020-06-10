502b28ebf365ba2463d7f2c12b811b59a6a09994
Oussama Bouguerne <oussama.bouguerne@gmail.com>
2020-06-10 13:33:54 +0100

    Configure app to serve static files & add cors middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
+
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 app.use(morgan((tokens, req, res) => {
     return [
         tokens.method(req, res),
