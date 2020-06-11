9f5994c342f73d024997693ab7a12cfa5856b15e
Oussama Bouguerne <oussama.bouguerne@gmail.com>
2020-06-11 14:34:52 +0100

    Exercise 3.19 - Solution to Phonebook database, step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -76,14 +76,8 @@ app.get("/api/persons/:id", (req, res, next) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
     const { name, number } = req.body
-
-    if (!name || !number) {
-        return res.status(400).json({
-            error: "Must provide name and number."
-        })
-    }
 
     const person = new Person({
         name,
@@ -94,6 +88,7 @@ app.post("/api/persons", (req, res) => {
         .then(savedPerson => {
             res.status(201).json(savedPerson)
         })
+        .catch(error => next(error))
 
 })
 
@@ -127,6 +122,8 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).send({ error: "malformatted id" })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).send({ error: error.message })
     }
 
     next(error)
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -13,11 +14,13 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     })
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String
+    name: { type: String, required: true, unique: true },
+    number: { type: String, required: true }
 })
+personSchema.plugin(uniqueValidator)
 
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
 personSchema.set('toJSON', {
     transform: (doc, returnedObj) => {
