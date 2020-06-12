74c62386939afe4c04f381d771bd7b7e8f73e7cb
Oussama Bouguerne <oussama.bouguerne@gmail.com>
2020-06-12 10:36:07 +0100

    Exercise 4.5 - Solution to helper functions and unit tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = blogs => {
     return blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+    return blogs.reduce((acc, blog) => {
+        if (blog.likes > acc.likes || !acc.likes) {
+            return blog
+        }
+
+        return acc
+    }, {})
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
